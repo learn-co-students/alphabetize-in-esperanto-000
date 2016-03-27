96520f48fca4a8501c1028817998c71a84b7101e
mendyismyname <m.seldowitz@gmail.com>
2016-03-27 13:44:17 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  result = arr.sort_by do |word|
+    word.chars.map do |letter|
+      alpha.index(letter)
+    end
+  end
 end
