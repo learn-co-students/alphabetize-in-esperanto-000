426c534cf685a3b634f72e9985bfefb67649ae8e
Lucas Fields <lqf2b8@gmail.com>
2016-10-30 14:14:36 +0000

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+# coding: utf-8
 def alphabetize(arr)
   # code here
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  result = arr.sort_by do |word|
+    word.chars.map do |letter|
+      alpha.index(letter)
+    end
+  end
 end
