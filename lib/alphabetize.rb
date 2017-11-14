ebebfcd4760454911c92354d95c307cd362df417
iwilliams83 <indirajamurath@gmail.com>
2017-11-14 00:18:52 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
   # code here
+  #arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+  esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by {
+    |word| word.split('').map{|letter| esp_alphabet.index(letter)}
+  }
+
 end
